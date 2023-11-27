e5ae8e969d41a9c5039acf478a91da94ef54885a
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2023-11-27 22:59:21 +0100

    initial commit

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class DatabaseContext : DbContext {
+    public DbSet<User> Users { get; set; }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {
+    }
+}
---
+++ Data/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities; 
+
+public class User {
+    [Key]
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
