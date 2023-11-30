90f3f0bff359e545c98c5d3ffa52beeefe4dc0bc
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2023-11-30 18:26:10 +0100

    Add popular and top-rated endpoints

--- Services/movie/IMovieService.cs
+++ Services/movie/IMovieService.cs
@@ -5,4 +5,6 @@ namespace Services.movie;
 public interface IMovieService {
     Task<List<MovieDto>> GetNowPlaying();
     Task<List<MovieDto>> GetTrending();
+    Task<List<MovieDto>> GetPopular();
+    Task<List<MovieDto>> GetTopRated();
 }
