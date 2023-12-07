e9a0997a608385578a55c2a930cc5c89f732527e
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2023-12-08 00:24:46 +0100

    Fix a niche scenario bug when calling getReviewsByMovieId for movies with no reviews would cause statuscode 500 instead of 200 with empty list

--- Data/dao/reviews/ReviewsDao.cs
+++ Data/dao/reviews/ReviewsDao.cs
@@ -30,31 +30,49 @@ public class ReviewsDao : IReviewsDao {
     }
 
     public async Task<GetMovieReviewsResponseDto> GetReviewsByMovieId(int movieId, int page, int pageSize) {
-        if (page < 1 || pageSize < 1) {
-            throw new Exception(ErrorMessages.InvalidPageOrPageSize);
+        try {
+            if (page < 1 || pageSize < 1) {
+                throw new Exception(ErrorMessages.InvalidPageOrPageSize);
+            }
+
+            int skipAmount = (page - 1) * pageSize;
+
+            List<ReviewEntity> reviewEntities = await _databaseContext.Reviews
+                .Where(entity => entity.MovieId == movieId)
+                .OrderByDescending(review => review.AuthoredAt)
+                .Skip(skipAmount)
+                .Take(pageSize)
+                .ToListAsync();
+
+            if (!reviewEntities.Any()) {
+                Console.WriteLine("No reviews found for movieId: " + movieId);
+            }
+
+            int totalReviews = await _databaseContext.Reviews
+                .Where(entity => entity.MovieId == movieId)
+                .CountAsync();
+
+            int totalPages = (int) Math.Ceiling((double) totalReviews / pageSize);
+
+            double averageRating = await _databaseContext.Reviews
+                .Where(reviewEntity => reviewEntity.MovieId == movieId)
+                .Select(entity => entity.Rating)
+                .AverageAsync();
+
+            return ReviewConverter.ToResponseDto(reviewEntities, totalPages, averageRating, totalReviews);
         }
-
-        int skipAmount = (page - 1) * pageSize;
-
-        List<ReviewEntity> reviewEntities = await _databaseContext.Reviews
-            .Where(entity => entity.MovieId == movieId)
-            .OrderByDescending(review => review.AuthoredAt)
-            .Skip(skipAmount)
-            .Take(pageSize)
-            .ToListAsync();
-
-        int totalReviews = await _databaseContext.Reviews
-            .Where(entity => entity.MovieId == movieId)
-            .CountAsync();
-
-        int totalPages = (int) Math.Ceiling((double) totalReviews / pageSize);
-
-        double averageRating = await _databaseContext.Reviews
-            .Where(reviewEntity => reviewEntity.MovieId == movieId)
-            .Select(entity => entity.Rating)
-            .AverageAsync();
-
-        return ReviewConverter.ToResponseDto(reviewEntities, totalPages, averageRating, totalReviews);
+        catch (Exception e) {
+            if (e.Message.Contains("Sequence contains no elements")) {
+                return new GetMovieReviewsResponseDto() {
+                    Reviews = new List<ReviewDto>(),
+                    AverageRating = 0,
+                    MovieId = movieId,
+                    TotalPages = 1,
+                    TotalReviews = 0
+                };
+            }
+            throw;
+        }
     }
 
     public async Task<List<ReviewDto>> GetReviewsByMovieId(int movieId) {
