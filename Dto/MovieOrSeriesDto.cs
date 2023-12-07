e1acc74e245ff8ce95624192f9644ef823b06cc1
Himal sharma <41054866+himal28924@users.noreply.github.com>
2023-12-08 00:56:38 +0100

    data for visualization  (#39)
    * Only want to send 10 popular movies but has a flaw
    
    * Can now get data for visualisation

--- Dto/MovieOrSeriesDto.cs
+++ Dto/MovieOrSeriesDto.cs
@@ -13,8 +13,27 @@ public class MovieOrSeriesDto
     [JsonPropertyName("poster_path")]
     public string? PosterPath { get; set; }
     
-    [JsonPropertyName("original_title")]
+    [JsonPropertyName("original_name")]
     public string? OriginalName { get; set; }
     
+    [JsonPropertyName("popularity")]
+    public double? Popularity { get; set; }
     
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+    
+    [JsonPropertyName("media_type")]
+    public string MediaType { get; set; }
+    
+    [JsonPropertyName("order")]
+    public int? Order { get; set; }
+    
+    [JsonPropertyName("release_date")]
+    public string? ReleaseDate { get; set; }
+    
+    [JsonPropertyName("first_air_date")]
+    public string? FirstAirDate { get; set; }
+    
+    [JsonPropertyName("genre_ids")]
+    public List<int> GenreIds { get; set; }
 }
---
+++ Dto/PersonMovieRolesDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dto;
+
+public class PersonMovieRolesDto
+{
+    [JsonPropertyName("lead_role")]
+    public int? LeadRoles { get; set; }
+    
+    [JsonPropertyName("supporting_role")]
+    public int? SupportingRoles { get; set; }
+    
+    [JsonPropertyName("guest_role")]
+    public int? GuestRoles { get; set; }
+    
+    [JsonPropertyName("crew_role")]
+    public int? CrewRoles { get; set; } 
+
+    [JsonPropertyName("not_specified")]
+    public int? NotSpecified { get; set; }
+}
